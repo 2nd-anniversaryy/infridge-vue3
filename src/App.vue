f8dbb1465edd88b6b4d7c1f826b4d23ab5d8a586
zerohyun.j <zerohyun.j@gmail.com>
2023-11-02 13:51:04 +0900

    [refactor] App.vue 파일 토큰 요청 -> router에서 네비게이션 가드 이용해 요청하도록 변경
    * beforeEnter를 통해 회원이면 회원의 냉장고 리스트 페이지, 비회원이면 인덱스 페이지로 이동하도록 구현

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,4 @@
-<script setup lang="ts">
-import { onMounted } from "vue";
-
-import { useMemberStore, useTokenStore } from "@/stores";
-import { log } from "@/utils";
-
-// setup
-const { getMember } = useMemberStore();
-const { getAccessToken } = useTokenStore();
-
-// method
-onMounted(async () => {
-  const result = await getAccessToken();
-  if (result) await getMember();
-});
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <RouterView />
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,48 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Index from '../views/Index.vue'
-import Layout from '../components/Layout.vue'
+import { createRouter, createWebHistory, useRouter } from "vue-router";
+import Index from "../views/Index.vue";
+import Layout from "../components/Layout.vue";
+import { useMemberStore, useTokenStore } from "@/stores";
+import { log } from "@/utils";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: "/",
       component: Layout,
       children: [
-        {path:'', component: Index},
-        {path:'member/fridges/list', component: ()=>import("../views/member/fridge/list/Index.vue")},
-        {path:'member/fridges/detail', component: ()=>import("../views/member/fridge/detail/Index.vue")}
-      ]
-    },
-  ]
-})
+        { path: "", component: Index, name: "index" },
+        {
+          path: "member/fridge/list",
+          component: () => import("../views/member/fridge/list/Index.vue"),
+          name: "fridgeList",
+          beforeEnter: (to, from) => {
+            const { isAuthentication } = useMemberStore();
+            if (!isAuthentication()) return { name: "index" };
+          },
+        },
+        {
+          path: "member/fridge/detail",
+          component: () => import("../views/member/fridge/detail/Index.vue"),
+        },
+      ],
+      beforeEnter: async (to, from) => {
+        const { getAccessToken } = useTokenStore();
+        const { isAuthentication, getMember } = useMemberStore();
 
-export default router
+        if (to.name !== "index") return;
+
+        // 회원이 아닐 경우 쿠키에 있는 리프레쉬토큰으로 엑세스토큰 요청
+        if (!isAuthentication()) {
+          const accessToken = await getAccessToken();
+          if (accessToken) await getMember();
+        }
+
+        // 회원일 경우 냉장고 리스트, 회원이 아닐 경우 인덱스 페이지
+        if (isAuthentication()) return { name: "fridgeList" };
+      },
+    },
+  ],
+});
+
+export default router;
